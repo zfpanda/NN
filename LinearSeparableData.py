bd1cf1f8e40546cabec5b1f1f571239067deae37
zfpanda <48564757+zfpanda@users.noreply.github.com>
2019-12-05 14:59:21 +0800

    Add files via upload

--- LinearSeparableData.py
+++ LinearSeparableData.py
@@ -20,36 +20,44 @@ def makeLinearSeparableData(n_samples,n_features):
 # print(random.rand(2)*2 -1)
 
 
-def plotData(dataSet):
+def plotData(dataSet,classifier):
     ''' (array) -> figure
 
     Plot a figure of dataSet
 
     '''
-
+    
     import matplotlib.pyplot as plt 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_title('Linear separable data set')
     plt.xlabel('X')
     plt.ylabel('Y')
+    x = range(-10,10) 
+    y = (classifier[0] + classifier[1]*x)/classifier[2]*(-1)
     labels = array(dataSet[:,2])
     idx_1 = where(dataSet[:,2]==1)
     p1 = ax.scatter(dataSet[idx_1,0], dataSet[idx_1,1], marker='o', color='g', label=1, s=20)
     idx_2 = where(dataSet[:,2]==-1)
     p2 = ax.scatter(dataSet[idx_2,0], dataSet[idx_2,1], marker='x', color='r', label=2, s=20)
     plt.legend(loc = 'upper right')
+     
+    plt.plot(x,y)
+
+
     plt.show()
+
 data = makeLinearSeparableData(100,2)
-f = plotData(data)
+
 
 X = data[...,[0,1]]
 y = data[...,[-1]]
 
-ppn = Perceptron(eta = 0.1, n_iter = 10)
+ppn = Perceptron(eta = 0.1, n_iter = 50)
 ppn.fit(X,y)
-
-plt.plot(range(1,len(ppn.errors) + 1), ppn.errors, marker = 'o')
-plt.xlabel('Epoches')  
-plt.ylabel('Number of misclassification')
-plt.show()
+# print(ppn.fit(X,y).w)
+f = plotData(data,ppn.fit(X,y).w)
+# plt.plot(range(1,len(ppn.errors) + 1), ppn.errors, marker = 'o')
+# plt.xlabel('Epoches')  
+# plt.ylabel('Number of misclassification')
+# plt.show()
